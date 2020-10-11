95a10d43ec650b48f32c8de2c5e281c32e4d50b7
Mayur8933 <mp326395@gmail.com>
2020-10-11 14:41:55 +0530

    changes Updated

--- store/views/home.py
+++ store/views/home.py
@@ -9,10 +9,31 @@ class Index(View):
 
     def post(self,request):
         product = request.POST.get('product')
-        print(product)
+        remove = request.POST.get('remove')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                if remove:
+                    if quantity <= 1:
+                        cart.pop(product)
+                    else:
+                        cart[product] = quantity - 1
+                else:
+                    cart[product] = quantity + 1
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+        request.session['cart'] = cart
+        print('cart',cart)
         return redirect('homepage')
 
     def get(self,request):
+        cart = request.session.get('cart')
+        if not cart:
+            request.session['cart'] = {}
         products = None
         categories = Category.get_all_categories()
         categoryId = request.GET.get('category')
